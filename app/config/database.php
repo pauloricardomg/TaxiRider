2c2a0a107cb8a5686acd482c72fcfea7bdafd195
Paulo Ricardo Motta Gomes <pauloricardomg@gmail.com>
2012-02-22 15:07:51 +0000

    initial commit - cakephp + db connection up and running

---
+++ app/config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'postgres',
+		'persistent' => true,
+		'host' => 'localhost',
+		'login' => 'taxirider',
+		'password' => 'taxirider',
+		'database' => 'taxirider',
+	);
+}
+?>
