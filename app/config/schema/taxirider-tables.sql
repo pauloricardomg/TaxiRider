33a60325ebab743ce9dc3a0685ef510753f4c588
Paulo Ricardo Motta Gomes <pauloricardomg@gmail.com>
2012-02-23 00:17:46 +0000

    modified postgresql schema according to cakephp conventions

--- app/config/schema/taxirider-tables.sql
+++ app/config/schema/taxirider-tables.sql
@@ -20,37 +20,41 @@ CREATE TABLE requests (
 		status SMALLINT NOT NULL DEFAULT 0,
 		passenger_boarded BOOL DEFAULT NULL,
 		passenger_picked BOOL DEFAULT NULL,
-		created_ts TIMESTAMP,
-		closed_ts TIMESTAMP DEFAULT NULL,
+		created TIMESTAMP DEFAULT NULL,
+		modified TIMESTAMP DEFAULT NULL,
+		closed TIMESTAMP DEFAULT NULL,
 		review TEXT DEFAULT NULL,
 		anonymous_review BOOL DEFAULT FALSE );
 SELECT AddGeometryColumn( 'requests', 'start_position', -1, 'POINT', 2 );
 SELECT AddGeometryColumn( 'requests', 'end_position', -1, 'POINT', 2 );
 
-DROP TRIGGER IF EXISTS request_open ON requests;
+
 DROP TRIGGER IF EXISTS request_update ON requests;
 
 CREATE OR REPLACE FUNCTION close_request() RETURNS TRIGGER AS $close_request$
     BEGIN
         IF NEW.passenger_boarded = FALSE AND NEW.passenger_picked = FALSE THEN
             NEW.status = 5;
-            NEW.closed_ts := current_timestamp;
+            NEW.closed := current_timestamp;
         END IF;
         RETURN NEW;
     END;
 $close_request$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION open_request() RETURNS trigger AS $open_request$
-    BEGIN
-        -- Time stamp request
-        NEW.created_ts := current_timestamp;
-        RETURN NEW;
-    END;
-$open_request$ LANGUAGE plpgsql;
+-- Creation timestamp already managed by CakePHP --
+-- DROP TRIGGER IF EXISTS request_open ON requests;
+-- CREATE OR REPLACE FUNCTION open_request() RETURNS trigger AS $open_request$
+--     BEGIN
+--         -- Time stamp request
+--         NEW.created_ts := current_timestamp;
+--         RETURN NEW;
+--     END;
+-- $open_request$ LANGUAGE plpgsql;
 
-CREATE TRIGGER request_open
-BEFORE INSERT ON requests
-    FOR EACH ROW EXECUTE PROCEDURE open_request();
+-- Already manage by CakePHP --
+-- CREATE TRIGGER request_open
+-- BEFORE INSERT ON requests
+--     FOR EACH ROW EXECUTE PROCEDURE open_request();
 
 CREATE TRIGGER request_update
 BEFORE UPDATE ON requests
