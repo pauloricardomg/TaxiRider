79696b18a06dff4fed862eace470e9d945149232
Paulo Ricardo Motta Gomes <pauloricardomg@gmail.com>
2012-02-27 02:42:32 +0000

    now inserting a point with the correct srid

--- app/config/schema/taxirider-samplequeries.sql
+++ app/config/schema/taxirider-samplequeries.sql
@@ -2,12 +2,14 @@
 DELETE FROM passengers;
 DELETE FROM taxis;
 
-INSERT INTO passengers (nome, position) VALUES ('joao', ST_GeomFromText('POINT(0 0)'));
-INSERT INTO taxis (nome, position) VALUES ('marcelo', ST_GeomFromText('POINT(1 1)'));
+INSERT INTO passengers (name, position) VALUES ('jorge', ST_GeomFromText('POINT(-34.608417 -58.373161)', 4326));
 
-INSERT INTO requests (id_passenger, id_taxi) VALUES (1, 1) RETURNING id;
+-- INSERT INTO passengers (nome, position) VALUES ('joao', ST_GeomFromText('POINT(0 0)'));
+-- INSERT INTO taxis (nome, position) VALUES ('marcelo', ST_GeomFromText('POINT(1 1)'));
 
-UPDATE requests SET status=2 WHERE id=1;
-UPDATE requests SET passenger_boarded=FALSE WHERE id=1;
-UPDATE requests SET passenger_picked=FALSE WHERE id=1;
-select * from requests;
+-- INSERT INTO requests (id_passenger, id_taxi) VALUES (1, 1) RETURNING id;
+
+-- UPDATE requests SET status=2 WHERE id=1;
+-- UPDATE requests SET passenger_boarded=FALSE WHERE id=1;
+-- UPDATE requests SET passenger_picked=FALSE WHERE id=1;
+-- select * from requests;
