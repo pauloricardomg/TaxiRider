1809ac1d802c2da68fe03a59545f9c7395e18442
Paulo Ricardo Motta Gomes <pauloricardomg@gmail.com>
2012-02-28 22:44:43 +0000

    moved point conversion to the AppModel

--- app/app_model.php
+++ app/app_model.php
@@ -31,4 +31,34 @@
  * @subpackage    cake.app
  */
 class AppModel extends Model {
+	
+	/**
+	* Convert a PostGis text point in format "POINT(lng lat)" to "lat, lng" string
+	* @param string $postgisPoint original point returned by postGis
+	* @return string the parsed point
+	*/
+	function convertToCsv($postgisPoint){
+		preg_match("/POINT\((-?[0-9]*\.[0-9]*) (-?[0-9]*\.[0-9]*)\)/", $postgisPoint, $result);
+		unset($result[0]); //remove first element from array
+		//reverse array, because postgis works with lng-lat, while gmaps with lat-lng
+		return implode(",", array_reverse($result));
+	}
+	
+	/**
+	 * Convert a latitude and longitude to a PostGis point
+	 * @param string $postgisPoint original point returned by postGis
+	 * @return string the parsed point
+	 */
+	function convertToPostGisPoint($latlng){
+		//Remove leading parentheses
+		$latlng = str_replace(array('(',')'), '', $latlng);
+	
+		//Explode and trim latitude and longitude
+		list ($lat, $lng) = array_map('trim',explode(",",$latlng));
+	
+		$expr = "ST_GeomFromText('POINT(".$lng." ".$lat.")', 4326)";
+		$db = $this->getDataSource();
+		return $db->expression($expr);
+	}
+	
 }
